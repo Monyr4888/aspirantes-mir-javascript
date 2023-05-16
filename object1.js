b27326975841b1800cf25776ef5bbbee408794a3
Monyr82 <monyr82@gmail.com>
2023-05-15 20:17:41 -0500

    Cambios en la rama develop object1.js

---
+++ object1.js
@@ -0,0 +1,25 @@
+const pedro = {
+    nombre: "Pedro Perez",
+    edad: 30,
+    hobbies: ['programar', 'squash', 'piano'] 
+    
+  }
+  pedro.estatura = 1.8;
+  delete pedro.activo
+
+  for (let key in pedro) {
+    console.log(key + ": " + pedro[key]);
+  }
+
+  pedro.saluda = function() {
+    return "Hola, me llamo " + this.nombre;
+  };
+
+ 
+
+console.log(pedro.edad);  //30
+console.log(pedro.estatura);// imprime 1.8
+console.log(pedro); //Elimina la propiedad con llave activo
+console.log(pedro.saluda());
+
+
